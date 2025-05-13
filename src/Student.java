10f8d7354ee8fdca9ab837e6a87e0f4d47ebeaf3
luaiuyuki <luanguyen0309@gmail.com>
2025-05-13 11:40:45 +0700

    Cập nhật toàn bộ mã nguồn và khôi phục README.md

--- src/Student.java
+++ src/Student.java
@@ -1,7 +1,14 @@
 public class Student {
-    private String studentID;
-    private String fullName;
-    private String dateOfBirth;
-    private String className;
-    
+    public String id;  
+    public String name;  
+    public String dob;  
+    public String gender;  
+
+    // Constructor
+    public Student(String id, String name, String dob, String gender) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.gender = gender;
+    }
 }
