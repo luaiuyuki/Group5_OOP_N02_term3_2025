85e331a8ae87026cda9d298165cb53bc41a5da97
luaiuyuki <luanguyen0309@gmail.com>
2025-05-18 14:14:53 +0700

    Hoài Lụa

--- src/App.java
+++ src/App.java
@@ -1,12 +1,12 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Running User tests:");
-        TestUser.main(null); // Gọi lớp TestUser chạy
+        TestUser.main(null); 
 
         System.out.println("\nRunning Time tests:");
-        TestTime.main(null); // Gọi lớp TestTime chạy
+        TestTime.main(null); 
 
         System.out.println("\nRunning Recursion tests:");
-        TestRecursion.main(null); // Gọi lớp TestRecursion chạy
+        TestRecursion.main(null); 
     }
 }
