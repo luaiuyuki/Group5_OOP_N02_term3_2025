ef2c468542365564ae624da29ccca1bc63a8d603
luaiuyuki <Luanguyen0309@gmail.com>
2025-05-23 21:29:16 +0700

    Delete Test directory

--- Test/TestUser.java
+++
@@ -1,9 +0,0 @@
-// TestUser.java
-public class TestUser {
-    public static void main(String[] args) {
-        User user = new User("alice", "pass123", "admin");
-        System.out.println(user);
-        user.setPassword("newpass");
-        System.out.println("Updated password: " + user.getPassword());
-    }
-}
