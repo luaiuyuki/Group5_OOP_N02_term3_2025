373c4452c08493c636e17a9c98a087168fe008d9
luaiuyuki <Luanguyen0309@gmail.com>
2025-05-24 18:37:09 +0700

    Update TestStudent.java

--- test/TestStudent.java
+++ test/TestStudent.java
@@ -1,17 +1,14 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-// Lớp StudentTest để kiểm tra lớp Student bằng cách nhập dữ liệu từ bàn phím
 public class StudentTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Student> studentList = new ArrayList<>();
 
-        // Nhập số lượng sinh viên
         System.out.print("Enter the number of students: ");
         int n = Integer.parseInt(sc.nextLine());
 
-        // Nhập thông tin cho từng sinh viên
         for (int i = 0; i < n; i++) {
             System.out.println("\nEnter information for student " + (i + 1) + ":");
 
@@ -30,17 +27,17 @@ public class StudentTest {
             System.out.print("Major: ");
             String major = sc.nextLine();
 
-            // Tạo đối tượng Student và thêm vào danh sách
             Student s = new Student(id, name, gender, dob, major);
             studentList.add(s);
         }
 
-        // Hiển thị danh sách sinh viên sau khi nhập
         System.out.println("\n===== STUDENT LIST =====");
+
         for (Student s : studentList) {
-            System.out.println(s);
+            s.printStudentInfo();  // in thông tin chi tiết từng sinh viên
         }
 
         sc.close();
     }
 }
+
